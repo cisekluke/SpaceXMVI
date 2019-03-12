d011b2275b947ae48b3a7961e83423aa43448bde
danielr <daniel.rodak@untitledkingdom.com>
2019-03-12 11:38:34 +0100

    added comments to view robot

--- app/src/test/java/co/untitledkingdom/spacexmvi/MainViewRobot.kt
+++ app/src/test/java/co/untitledkingdom/spacexmvi/MainViewRobot.kt
@@ -5,20 +5,26 @@ import io.reactivex.subjects.PublishSubject
 
 class MainViewRobot(mainViewModel: MainViewModel) : BaseMviViewRobot<MainView, MainViewModel, MainViewState>(mainViewModel) {
 
+    /** typical subjects to emit simulated actions */
     private val buttonClickSubject = PublishSubject.create<Boolean>()
     private val clearClickSubject = PublishSubject.create<Boolean>()
 
+    /** override array were you will be storing your rendered states */
     override val renderedStates = arrayListOf<MainViewState>()
+
+    /** override view which will be attached to view model */
     override val mainView = object : MainView {
         override fun emitClearButton(): Observable<Boolean> = clearClickSubject
 
         override fun emitButtonClick(): Observable<Boolean> = buttonClickSubject
 
         override fun render(mainViewState: MainViewState) {
+            /** add states to previously overridden states holder */
             renderedStates.add(mainViewState)
         }
     }
 
+    /** public methods to simulate actions */
     fun emitButtonClick() {
         buttonClickSubject.onNext(true)
     }
