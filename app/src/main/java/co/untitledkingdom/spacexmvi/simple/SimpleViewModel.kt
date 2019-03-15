765c0fb47cea1238e24f51d15ec7ea8f3f80bffe
danielr <daniel.rodak@untitledkingdom.com>
2019-03-15 17:01:47 +0100

    simple view model

--- app/src/main/java/co/untitledkingdom/spacexmvi/simple/SimpleViewModel.kt
+++ app/src/main/java/co/untitledkingdom/spacexmvi/simple/SimpleViewModel.kt
@@ -1,7 +1,15 @@
 package co.untitledkingdom.spacexmvi.simple
 
-import android.arch.lifecycle.ViewModel;
+import co.untitledkingdom.spacexmvi.base.BaseMviIntent
+import co.untitledkingdom.spacexmvi.base.BaseViewModel
+import io.reactivex.Observable
 
-class SimpleViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class SimpleViewModel : BaseViewModel<SimpleViewState, SimpleView, SimpleAction>() {
+
+    override val defaultViewState: SimpleViewState
+        get() = SimpleViewState()
+
+    override fun <I : BaseMviIntent> intentToAction(intent: I): Observable<SimpleAction> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 }
