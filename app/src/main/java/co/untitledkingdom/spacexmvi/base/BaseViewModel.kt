024fbe2c918ccdc852edd1f7337f85da181bb423
danielr <daniel.rodak@untitledkingdom.com>
2019-03-28 17:14:33 +0100

    added doc for viewmodel

--- app/src/main/java/co/untitledkingdom/spacexmvi/base/BaseViewModel.kt
+++ app/src/main/java/co/untitledkingdom/spacexmvi/base/BaseViewModel.kt
@@ -7,29 +7,78 @@ import io.reactivex.Observable
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.subjects.BehaviorSubject
 
+/**
+ * Base class to implement for ViewModels to follow our MVVMI architecture approach. This one
+ * is responsible to act as the middleman between view and model.
+ *
+ * @param S stands for our ViewState data class
+ * @param V is our View interface
+ * @param A sealed class that holds actions
+ */
 abstract class BaseViewModel<S : BaseMviViewState, V : BaseMviView<S, *>, A : BaseMviAction<S>> :
     ViewModel() {
 
+    /**
+     * It needs to be overridden due to first state render.
+     *
+     * @return ViewState that should be displayed at first open
+     */
     protected abstract val defaultViewState: S
 
     private lateinit var view: V
 
+    /**
+     * Subscription holder that needs to be managed by #bind and #unbind due to lifecycle
+     * changes.
+     */
     private val compositeDisposable = CompositeDisposable()
+
+    /**
+     * Subject that holds states and remembers last state.
+     *
+     * @return #cold subject that holds state
+     */
     private val stateSubject = BehaviorSubject.create<S>()
+
+    /**
+     * Internal flag used to know if we have already subscribed or should subscribe to state
+     * subject.
+     */
     private var subscribed = false
     private var isInitialized = false
 
-    fun isAlreadyInitialized() = isInitialized
+    /**
+     * Method used to know if it's new instance of ViewModel. It's being used to inform
+     * View if state instance should be restored.
+     *
+     * @return Boolean says if new ViewModel has been created
+     */
+    internal fun isAlreadyInitialized() = isInitialized
 
-    fun setInitialViewState(viewState: S) {
+    /**
+     * If state has been restored by View this method will set initial value to our subject.
+     *
+     * @param viewState initial state being loaded from the Bundle
+     */
+    internal fun setInitialViewState(viewState: S) {
         stateSubject.onNext(viewState)
     }
 
+    /**
+     * Happens once during ViewModel life, at the beginning so here we mark it as created
+     * and initialized.
+     *
+     * @param view Setting View.
+     */
     internal fun attachView(view: V) {
         this.view = view
         isInitialized = true
     }
 
+    /**
+     * This is being called in activity's onStart. If there was no subscription, it will
+     * call proper methods. Otherwise it will just call render.
+     */
     internal fun bind() {
         if (!subscribed) {
             subscribeStates(mapIntents())
@@ -39,6 +88,12 @@ abstract class BaseViewModel<S : BaseMviViewState, V : BaseMviView<S, *>, A : Ba
         renderStates()
     }
 
+    /**
+     * Intents observer is being subscribed to state subject. Important here is to not duplicate
+     * subscription because it will cause emitting multiple intents.
+     *
+     * @param intents intents coming from View
+     */
     private fun subscribeStates(intents: Observable<A>) {
         intents.scan(getViewState(), this::reduce)
             .replay(1)
@@ -48,17 +103,47 @@ abstract class BaseViewModel<S : BaseMviViewState, V : BaseMviView<S, *>, A : Ba
         subscribed = true
     }
 
-    fun getViewState(): S = stateSubject.value ?: defaultViewState
+    /**
+     * Get value from subject. It returns value or if there's no value then, will use default one
+     * provided inside feature ViewModel.
+     *
+     * @return last view state
+     */
+    internal fun getViewState(): S = stateSubject.value ?: defaultViewState
 
+    /**
+     * Merging out last state with the new one. This one reduces amount of newly created ViewStates
+     * usually by coping previous one.
+     *
+     * @param previousState last emitted state
+     * @param partialState new incoming state
+     * @return reduced, merged ViewState
+     */
     private fun reduce(previousState: S, partialState: A): S =
         partialState.reduce(previousState)
 
+    /**
+     * Transforming incoming intent from View to Action.
+     *
+     * @return Observable that emits Actions
+     */
     private fun mapIntents(): Observable<A> =
         view.emitIntent()
             .flatMap { intentToAction(it) }
 
+    /**
+     * Override this method to assign which action should be perform due to incoming event.
+     * Here you should call Modelm Repository or any business layer object if needed.
+     *
+     * @param intent users interaction coming from the View
+     * @return Observable that emits Actions
+     */
     protected abstract fun <I : BaseMviIntent> intentToAction(intent: I): Observable<A>
 
+    /**
+     * Adding custom subscriptions to our Disposable to be confident that customs are being cleared
+     * at the same time as intents with action.
+     */
     private fun subscribeWithoutStates() {
         compositeDisposable.add(
             view.emitIntentWithoutAction()
@@ -66,8 +151,19 @@ abstract class BaseViewModel<S : BaseMviViewState, V : BaseMviView<S, *>, A : Ba
         )
     }
 
+    /**
+     * Override this when you need to create custom subscription that shouldn't return Action,
+     * like for example Navigation intent.
+     * Here you should call Model, Repository or any business layer object if needed.
+     *
+     * @param intent users interaction coming from the View
+     */
     protected open fun <I : BaseMviIntent> intentWithoutAction(intent: I) {}
 
+    /**
+     * Pushing received states to the View, to perform UI action due to received intent and
+     * processed action.
+     */
     @MainThread
     private fun renderStates() {
         compositeDisposable.add(
@@ -78,13 +174,29 @@ abstract class BaseViewModel<S : BaseMviViewState, V : BaseMviView<S, *>, A : Ba
         )
     }
 
+    /**
+     * This one is to simplify creating Observable for simple intents that requires only Action
+     * without any additional Model layer calls.
+     *
+     * @param action our Action that needs to be transformed
+     * @return given Action transformed into Observable
+     */
     protected fun just(action: A): Observable<A> = Observable.just(action)
 
+    /**
+     * Clears subscriptions to avoid memory leaks. This one will be called in onStop.
+     * It's open to override but has @CallSuper annotation to be sure that internal subscription
+     * were cleared.
+     */
     @CallSuper
     internal open fun unbind() {
         compositeDisposable.clear()
     }
 
+    /**
+     * This one is being used because ViewModel survives orientation changes but View doesn't
+     * so it needs to resubscribe.
+     */
     internal fun unsubscribe() {
         subscribed = false
     }
